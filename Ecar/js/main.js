d2b192e7359a48f12f6f8409d22e25b1b06591d6
potatodzen <potatodzen@gmail.com>
2017-07-12 21:26:22 +0300

    прикрутив попапи

--- Ecar/js/main.js
+++ Ecar/js/main.js
@@ -1,34 +1,14 @@
-	$(document).ready(function() {
-	// $('body').css({
-	//     'font-family': 'LatoWeb,' + 'sans-serif'
-	// 	});
-
-	//SVG Fallback - якщо браузер не загрузить свг
-	// if(!Modernizr.svg) {
-	// 	$("img[src*='svg']").attr("src", function() {
-	// 		return $(this).attr("src").replace(".svg", ".png");
-	// 	});
-	// };
-
-
-
-	// HUMBURGER
-	// $('.burger-menu').on('click', function(){
-	//     var burger = ('.burger-menu'),
-	//         headnav = ('.head-nav'),
-	//         navigate = ('.navigation')
-
-	//     if($(burger).hasClass('menu-off')){
-	//         $(burger).removeClass('menu-off').addClass('menu-on');
-	//         $(headnav).addClass('show');
-	//         $(navigate).addClass('show-menu');
-	//         $('body').css('overflow', 'hidden');
-
-	//     }else{
-
-	//         $(burger).addClass('menu-off').removeClass('menu-on');
-	//         $(headnav).removeClass('show');
-	//         $(navigate).removeClass('show-menu');   
-	//         $('body').css('overflow', 'auto');
-	//     }
+$(document).ready(function() {
+	$('.fancybox').fancybox({
+            padding: 0,
+            openEffect  : 'none',
+            closeEffect : 'none',
+            nextEffect  : 'none',
+            prevEffect  : 'none',
+            helpers: {
+	            overlay: {
+	              locked: false
+	            }
+            }
+        });
 });
