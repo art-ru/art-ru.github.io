1b815da90be188090de4fd7a1308444ce90a1eac
potatodzen <potatodzen@gmail.com>
2017-07-14 13:35:18 +0300

    замінив срипт попапів
    видалив fancybox (баг з прокруткою в скролі) і замінив на magnificPopup

--- Ecar/js/main.js
+++ Ecar/js/main.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
-	$('.fancybox').fancybox({
-            padding: 0,
-            openEffect  : 'none',
-            closeEffect : 'none',
-            nextEffect  : 'none',
-            prevEffect  : 'none',
-            helpers: {
-	            overlay: {
-	              locked: false
-	            }
-            }
-        });
+	$('.g-btn, .confident-btn').magnificPopup({
+		tClose: 'Закрыть',
+	});
 });
